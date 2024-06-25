65bbca5c76e2eeb34c39d1fb09bf75119159854a
gi hwan <tjdrlghks0@gmail.com>
2024-06-25 10:34:22 +0900

    프로그램 수정

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,6 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-dvh">
       <header className="px-4 lg:px-6 h-14 flex items-center">
-        <Link className="flex items-center justify-center" href="#">
-          <MountainIcon className="size-6" />
-          <span className="sr-only">Graphic East</span>
-        </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <Link 
             className="text-sm font-medium hover:underline underline-offset-4" 
